a6991257c4ec422204fc78fd9edc2b7dddcec962
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2019-08-27 12:53:30 -0500

    add send sms

--- Services/TwilioClient.php
+++ Services/TwilioClient.php
@@ -12,20 +12,63 @@ class TwilioClient
     /** @var Client  */
     protected $service;
 
+    const MESSAGE_SMS = 'sms';
+    const MESSAGE_WHATSAPP = 'whatsapp';
+
+    /**
+     * TwilioClient constructor.
+     * @param $twilio_sid
+     * @param $twilio_token
+     */
     public function __construct($twilio_sid, $twilio_token)
     {
         // Create an Twilio client object
         $this->service = new Client($twilio_sid, $twilio_token);
     }
 
+    /**
+     * @param $to
+     * @param $from
+     * @param $body
+     * @return mixed
+     */
     public function sendWhatsAppMessage($to, $from, $body)
     {
+        return $this->sendMessage($to, $from, $body, self::MESSAGE_WHATSAPP);
+    }
+
+    /**
+     * @param $to
+     * @param $from
+     * @param $body
+     * @return mixed
+     */
+    public function sendSMSMessage($to, $from, $body)
+    {
+        return $this->sendMessage($to, $from, $body, self::MESSAGE_SMS);
+    }
+
+    /**
+     * @param $to
+     * @param $from
+     * @param $body
+     * @param $type
+     * @return mixed
+     */
+    private function sendMessage($to, $from, $body, $type)
+    {
+        if ($type == self::MESSAGE_WHATSAPP) {
+            $to = "whatsapp:{$to}";
+            $from =  "whatsapp:{$from}";
+        }
+
         $message = $this->service->messages
-            ->create("whatsapp:{$to}", // to
-                array(
-                    "from" => "whatsapp:{$from}",
+            ->create(
+                $to, // to
+                [
+                    "from" => $from,
                     "body" => $body
-                )
+                ]
             );
 
         return $message;
